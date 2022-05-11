652e05733ea07132fe045050d31d1e53b642b25b
RohnitShriyan05 <rohnitshriyan23@gmail.com>
2022-05-11 12:59:56 +0530

    Changed the first channel on opening

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ export const userContext = React.createContext(null);
 function App() {
   const [user, setUser] = useState({});
   const [login, setLogin] = useState(0);
-  const [currentChannelId, setCurrentChannelId] = useState("62761175434e2924bc245d7f");
-  const [currentChannel, setCurrentChannel] = useState("General");
+  const [currentChannelId, setCurrentChannelId] = useState("");
+  const [currentChannel, setCurrentChannel] = useState("Select a channel");
   const [sidebarToggle, setSidebarToggle] = useState(true);
   return (
     <div className="App bg-chatbg">
